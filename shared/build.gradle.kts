d9678845e4741972eda475f0c045eb363f940e59
ba la~ ba la~ <8428270+ohyooo@users.noreply.github.com>
2023-12-09 21:15:13 +0800

    Update build.gradle.kts

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -62,7 +62,7 @@ android {
         minSdk = libs.versions.min.sdk.get().toInt()
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
 }
